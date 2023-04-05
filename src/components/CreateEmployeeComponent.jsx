d3ef94bfb73f40bc547da4f0dc805cf82c2f9e51
Assadullah <128569682+asad74464@users.noreply.github.com>
2023-04-05 13:06:37 +0430

    using navigate instead of push

--- src/components/CreateEmployeeComponent.jsx
+++ src/components/CreateEmployeeComponent.jsx
@@ -1,99 +1,84 @@
-import React, { Component } from "react";
+import React, { useState } from "react";
+import { useNavigate } from "react-router-dom";
 
-class CreateEmployeeComponent extends Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      firstName: "",
-      lastName: "",
-      emailId: "",
-    };
+const CreateEmployeeComponent = () => {
+  const [employee, setEmployee] = useState({
+    firstName: "",
+    lastName: "",
+    emailId: "",
+  });
 
-    this.changeFirstNameHandler = this.changeFirstNameHandler.bind(this);
-    this.changeLastNameHandler = this.changeLastNameHandler.bind(this);
-    this.saveEmployee=this.saveEmployee.bind(this);
-    this.cancel=this.cancel.bind(this); // bind cancel method
+  const navigate = useNavigate();
 
-  }
-
-  saveEmployee = (e) => {
-    e.preventDefault();
-    let employee = {firstName: this.state.firstName, lastName: this.state.lastName, emailId: this.state.emailId};
-    console.log('employee => ' + JSON.stringify(employee));
-  }
-  changeFirstNameHandler = (event) => {
-    this.setState({ firstName: event.target.value });
+  const handleInputChange = (event) => {
+    const { name, value } = event.target;
+    setEmployee({ ...employee, [name]: value });
   };
 
-  changeLastNameHandler = (event) => {
-    this.setState({ lastName: event.target.value });
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    console.log("employee => ", employee);
   };
 
-  changeEmailHandler = (event) => {
-    this.setState({ emailId: event.target.value });
+  const handleCancel = () => {
+    navigate("/employees");
   };
 
-  cancel() { // define cancel method
-    this.props.history.push('/employees');
-  }
-  render() {
-    return (
-      <div>
-        <div className="container mt-5">
-          <div className="row">
-            <div className="card col-md-6 offset-md-3 offset-md-3">
-              <h3 className="text-center mt-3">Add Employee</h3>
-              <div className="card-body">
-                <form>
-                  <div className="form-group">
-                    <label>First Name</label>
-                    <input
-                      placeholder="First Name"
-                      name="firstName"
-                      className="form-control"
-                      value={this.state.firstName}
-                      onChange={this.changeFirstNameHandler}
-                    />
-                  </div>
-                  <div className="form-group">
-                    <label>Last Name</label>
-                    <input
-                      placeholder="Last Name"
-                      name="lastName"
-                      className="form-control"
-                      value={this.state.lastName}
-                      onChange={this.changeLastNameHandler}
-                    />
-                  </div>
-                  <div className="form-group">
-                    <label>Email Id</label>
-                    <input
-                      placeholder="Email Address"
-                      name="emailId"
-                      className="form-control"
-                      value={this.state.emailId}
-                      onChange={this.changeEmailHandler}
-                    />
-                  </div>
-                  <button
-                    className="btn btn-success"
-                    onClick={this.saveOrUpdateEmployee}>
-                    Save
-                  </button>
-                  <button
-                    className="btn btn-danger"
-                    onClick={this.cancel.bind(this)}
-                    style={{ marginLeft: "10px" }}>
-                    Cancel
-                  </button>
-                </form>
-              </div>
+  return (
+    <div>
+      <div className="container mt-5">
+        <div className="row">
+          <div className="card col-md-6 offset-md-3 offset-md-3">
+            <h3 className="text-center mt-3">Add Employee</h3>
+            <div className="card-body">
+              <form onSubmit={handleSubmit}>
+                <div className="form-group">
+                  <label>First Name</label>
+                  <input placeholder="First Name"
+                    type="text"
+                    name="firstName"
+                    className="form-control"
+                    value={employee.firstName}
+                    onChange={handleInputChange}
+                  />
+                </div>
+                <div className="form-group">
+                  <label>Last Name</label>
+                  <input placeholder="Last Name"
+                    type="text"
+                    name="lastName"
+                    className="form-control"
+                    value={employee.lastName}
+                    onChange={handleInputChange}
+                  />
+                </div>
+                <div className="form-group">
+                  <label>Email Id</label>
+                  <input placeholder="example@gmail.com"
+                    type="email"
+                    name="emailId"
+                    className="form-control"
+                    value={employee.emailId}
+                    onChange={handleInputChange}
+                  />
+                </div>
+                <button className="btn btn-success" type="submit">
+                  Save
+                </button>
+                <button
+                  className="btn btn-danger"
+                  onClick={handleCancel}
+                  style={{ marginLeft: "10px" }}
+                >
+                  Cancel
+                </button>
+              </form>
             </div>
           </div>
         </div>
       </div>
-    );
-  }
-}
+    </div>
+  );
+};
 
 export default CreateEmployeeComponent;
