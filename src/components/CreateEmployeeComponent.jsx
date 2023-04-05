7998434568c44d4414b77b7696ef14e8a2f4d00f
Assadullah <128569682+asad74464@users.noreply.github.com>
2023-04-05 11:37:22 +0430

    margin top

--- src/components/CreateEmployeeComponent.jsx
+++ src/components/CreateEmployeeComponent.jsx
@@ -39,10 +39,10 @@ class CreateEmployeeComponent extends Component {
   render() {
     return (
       <div>
-        <div className="container">
+        <div className="container mt-5">
           <div className="row">
             <div className="card col-md-6 offset-md-3 offset-md-3">
-              <h3 className="text-center">Add Employee</h3>
+              <h3 className="text-center mt-3">Add Employee</h3>
               <div className="card-body">
                 <form>
                   <div className="form-group">
