0cbf2d19e4c8b21c4e3d7b2e4d096d0d3c60f7cf
Assadullah <128569682+asad74464@users.noreply.github.com>
2023-04-06 11:28:54 +0430

    function component instead of class component

--- src/components/ViewEmployeeComponent.jsx
+++ src/components/ViewEmployeeComponent.jsx
@@ -1,46 +1,42 @@
-import React, { Component } from "react";
+import React, { useEffect,useState } from "react";
+import { Link, useParams } from "react-router-dom";
 import EmployeeService from "../services/EmployeeService";
 
-class ViewEmployeeComponent extends Component {
-  constructor(props) {
-    super(props);
+function ViewEmployeeComponent(props) {
+  const { id } = useParams();
+  const [employee, setEmployee] = useState({});
 
-    this.state = {
-      id: this.props.match.params.id,
-      employee: {},
-    };
-  }
-
-  componentDidMount() {
-    EmployeeService.getEmployeeById(this.state.id).then((res) => {
-      this.setState({ employee: res.data });
+  useEffect(() => {
+    EmployeeService.getEmployeeById(id).then((res) => {
+      setEmployee(res.data);
     });
-  }
+  }, [id]);
 
-  render() {
-    return (
-      <div>
-        <br></br>
-        <div className="card col-md-6 offset-md-3">
-          <h3 className="text-center"> View Employee Details</h3>
-          <div className="card-body">
-            <div className="row">
-              <label> Employee First Name: </label>
-              <div> {this.state.employee.firstName}</div>
-            </div>
-            <div className="row">
-              <label> Employee Last Name: </label>
-              <div> {this.state.employee.lastName}</div>
-            </div>
-            <div className="row">
-              <label> Employee Email ID: </label>
-              <div> {this.state.employee.emailId}</div>
-            </div>
+  return (
+    <div>
+      <br></br>
+      <div className="card col-md-6 offset-md-3">
+        <div className="card-header"><h3 className="text-center"> View Employee Details</h3></div>
+        <div className="card-body">
+          <div className="row">
+            <div><b>Employee First Name: </b></div>
+             <div>{employee.firstName}</div>
           </div>
+          <div className="row">
+            <b> Employee Last Name: </b>
+            <div> {employee.lastName}</div>
+          </div>
+          <div className="row">
+            <b> Employee Email ID: </b>
+            <div> {employee.emailId}</div>
+          </div>
+          <Link className="btn btn-primary my-2" to={"/"}>
+            Back to Home
+          </Link>
         </div>
       </div>
-    );
-  }
+    </div>
+  );
 }
 
 export default ViewEmployeeComponent;
